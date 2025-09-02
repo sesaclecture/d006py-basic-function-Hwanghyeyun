7ea53b26daa748092865f8750a0ce8c71b46bba2
hyeyun Hwang <hhyeyoun@naver.com>
2025-09-02 11:22:11 +0900

    0902과제

--- basic_func/basic_func.py
+++ basic_func/basic_func.py
@@ -1,26 +1,36 @@
 def add(a, b):
-    pass
+    return a + b
 
 
 def sub(a, b):
-    pass
+    return a -b
 
 
 def mul(a, b):
-    pass
+    return a *b
 
 
 def div(a, b):
-    pass
+    return a / b
 
 
 def power(base, pow):
-    pass
+    return base ** pow
 
 
 def square(base):
-    pass
+    return base*base
 
+def greet(이름="낯선자"):
+    return"안녕하신가" + f" {이름}!"
+    
 
 def greet(이름="낯선자", 나이=20):
-    pass
+    인사 = ""
+    if (나이 >= 40):
+        인사 = "안녕하십니까"
+    elif (나이 >= 20):
+        인사 = "안녕하신가"
+    else:
+        인사 = "안녕"
+    return 인사 + f" {이름}!"
